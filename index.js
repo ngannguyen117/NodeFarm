180c07a7dbdd4f027aa7e95b342f96e8aae7d2e7
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-02-14 09:26:39 -0800

    Moved replaceTemplate function to its own module

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replaceTemplate');
+
 /************************************ FILES ***********************************/
 // Blocking, synchronous way
 // const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -32,21 +34,6 @@ const url = require('url');
 
 
 /************************************ SERVER ***********************************/
-
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-
-  return output;
-};
 
 // Read data and load templates once before loading the page
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
